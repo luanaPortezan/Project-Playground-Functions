f85a1a54af9754f3597d6c45fd102ab60388db19
Luana Portezan <luanaportezan@gmail.com>
2022-07-26 14:55:43 -0300

    inicio do requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(mamifero1, mamifero2) {
+  if (manifero1 === true && manifero2 === true) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
